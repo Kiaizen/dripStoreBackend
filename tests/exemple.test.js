39098d1b14170a7e6124840d70d8d4dbd0ac5072
Kiaizen <kiaizenx@gmail.com>
2025-06-20 21:07:36 -0300

    feat: swagger doc e jest test

--- tests/exemple.test.js
+++ tests/exemple.test.js
@@ -1,4 +1,11 @@
-test('deve somar dois números', () => {
-  const soma = (a, b) => a + b;
-  expect(soma(2, 3)).toBe(5);
+const request = require('supertest');
+const app = require('../src/app');
+
+describe('POST /login', () => {
+  it('deve retornar 400 se o email for inválido', async () => {
+    const res = await request(app)
+      .post('/v1/user')
+      .send({ email: 'invalido', password: '123' });
+    expect(res.statusCode).toBe(400);
+  });
 });
